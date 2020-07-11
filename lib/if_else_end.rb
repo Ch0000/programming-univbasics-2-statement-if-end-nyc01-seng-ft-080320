0736f36b3d1b02e876a6ff55591c9a89813bdbbf
Christian P Andrews <cpandrews91@gmail.com>
2020-07-11 03:48:20 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,12 @@
 # Write your solution here
+#write an if...elsif...else...end
+#use statement modifiers to change default sequence
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
