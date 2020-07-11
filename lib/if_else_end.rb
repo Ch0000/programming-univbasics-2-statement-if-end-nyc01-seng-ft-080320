9a2512c7f77b6b8cdd66b8c5bd8ae4185045eda6
Christian P Andrews <cpandrews91@gmail.com>
2020-07-11 03:47:41 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
 # Write your solution here
+#write an if...elsif...else...end
+#use statement modifiers to change default sequence
+
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
